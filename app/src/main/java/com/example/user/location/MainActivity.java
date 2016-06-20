a86896fada7acb105c5ec98a73051b9c1b9c5b81
walkermannum1 <2469377884@qq.com>
2016-06-20 19:15:07 +0800

    Fix permission issue.

--- app/src/main/java/com/example/user/location/MainActivity.java
+++ app/src/main/java/com/example/user/location/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.user.location;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,15 +32,29 @@ public class MainActivity extends Activity {
             provider = LocationManager.GPS_PROVIDER;
         } else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
             provider = LocationManager.NETWORK_PROVIDER;
-        } else{
+        } else {
             Toast.makeText(this, "No location provider to use", Toast.LENGTH_SHORT).show();
             return;
         }
-        Location location = locationManager.getLastKnownLocation(provider);
-        if (location != null) {
-            showLocation(location);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            Location location = locationManager.getLastKnownLocation(provider);
+            if (location != null) {
+                showLocation(location);
+            }
+            locationManager.requestLocationUpdates(provider, 5000, 1, locationListener);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        locationManager.requestLocationUpdates(provider, 5000, 1, locationListener);
     }
 
     protected  void onDestroy() {
