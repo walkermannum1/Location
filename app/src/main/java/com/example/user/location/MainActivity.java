a4f092a79f58a2a76b40122ec17d442f3b7156bd
walkermannum1 <2469377884@qq.com>
2016-06-23 17:48:34 +0800

    Have some problem with baidu map yet.

--- app/src/main/java/com/example/user/location/MainActivity.java
+++ app/src/main/java/com/example/user/location/MainActivity.java
@@ -1,93 +1,39 @@
 package com.example.user.location;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.content.pm.ProviderInfo;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.widget.TextView;
-import android.widget.Toast;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 
-import java.util.List;
 
 public class MainActivity extends Activity {
 
-    private TextView positionTextView;
-    private LocationManager locationManager;
-    private String provider;
+    MapView mapView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        positionTextView = (TextView) findViewById(R.id.position_text_view);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        List<String> providerList = locationManager.getProviders(true);
-        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
-            provider = LocationManager.GPS_PROVIDER;
-        } else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
-            provider = LocationManager.NETWORK_PROVIDER;
-        } else {
-            Toast.makeText(this, "No location provider to use", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        try {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            Location location = locationManager.getLastKnownLocation(provider);
-            if (location != null) {
-                showLocation(location);
-            }
-            locationManager.requestLocationUpdates(provider, 5000, 1, locationListener);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        mapView = (MapView) findViewById(R.id.map_view);
     }
 
-    protected  void onDestroy() {
+    @Override
+    protected void onDestroy() {
         super.onDestroy();
-        if (locationManager != null) {
-            locationManager.removeUpdates(locationListener);
-        }
+        mapView.onDestroy();
     }
 
-    LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            showLocation(location);
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-    };
-
-    private void showLocation (Location location) {
-        String currentPosition = "latitude is: " + location.getLatitude()+"\n"+"longtitude is: " + location.getLongitude();
-        positionTextView.setText(currentPosition);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
 }
